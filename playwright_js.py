9548b209d63eb95559d87ac08155b2974223b428
rezyapkin26 <rezyapkin.petr1211@yandex.ru>
2025-12-23 13:57:27 +0300

    Углубление в Playwright 1

---
+++ playwright_js.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import sync_playwright
+
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login",
+              wait_until= 'networkidle'
+              )  ## команда для полной загрузки страницы
+
+    text = 'New Text'
+    page.evaluate(
+        f"""
+        const title = document.getElementById('authentication-ui-course-title-text')
+        title.textContent = '{text}'
+        """
+
+    )   ### команда для запуска js
+    page.wait_for_timeout(5000)
